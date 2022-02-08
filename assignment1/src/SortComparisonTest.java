dbdbd2a5c099e7dddb811a35af79b5690faaf1a8
weshaw <weshaw@tcd.ie>
2022-02-08 17:33:21 +0000

    set up for assignment 1

---
+++ assignment1/src/SortComparisonTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+class SortComparisonTest {
+
+    //~ Constructor ........................................................
+    @Test
+    public void testConstructor()
+    {
+        new SortComparison();
+    }
+
+    //~ Public Methods ........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Check that the methods work for empty arrays
+     */
+    @Test
+    public void testEmpty()
+    {
+    }
+
+
+    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
+    // be executed at least once from at least one test.
+
+    // ----------------------------------------------------------
+    /**
+     *  Main Method.
+     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
+     *
+     */
+    public static void main(String[] args)
+    {
+        //TODO: implement this method
+    }
+
+}
